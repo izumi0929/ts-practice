f9ae689202aa495881e9f09b93f15e1402205903
izumeeeel <izumi.watahiki@gmail.com>
2022-05-01 17:44:09 +0900

    add fizbuzz

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,12 +1,25 @@
-import { createInterface } from 'readline'
+// import { createInterface } from 'readline'
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
 
-rl.question('Input strings:', (line) => {
-  const result = BigInt(line)
-  console.log(`You input ${result}`)
-  rl.close()
-})
+// rl.question('Input strings:', (line) => {
+//   const result = BigInt(line)
+//   console.log(`You input ${result}`)
+//   rl.close()
+// })
+let fizbuzz = ''
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    fizbuzz += 'FizzBuzz '
+  } else if (i % 3 === 0) {
+    fizbuzz += 'Fizz '
+  } else if (i % 5 === 0) {
+    fizbuzz += 'Buzz '
+  } else {
+    fizbuzz += i + ' '
+  }
+}
+console.log(fizbuzz)
