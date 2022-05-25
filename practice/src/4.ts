611a595f1216580adfeaea36d45fc07802a08f8d
izumeeeel <izumi.watahiki@gmail.com>
2022-05-25 23:50:23 +0900

    add getFizzBuzzString

---
+++ practice/src/4.ts
@@ -0,0 +1,16 @@
+const getFizzBuzzString = (num: number) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz'
+  } else if (num % 3 === 0) {
+    return 'Fizz'
+  } else if (num % 5 === 0) {
+    return 'Buzz'
+  } else {
+    return num
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  const message = getFizzBuzzString(i)
+  console.log(message)
+}
