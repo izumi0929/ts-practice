afe8b8596229fed31f93d2a12deb636ce1e87cab
izumeeeel <izumi.watahiki@gmail.com>
2022-05-27 23:16:18 +0900

    add 6.8.1

---
+++ practice/src/6.8.1.ts
@@ -0,0 +1,29 @@
+export {}
+
+type HasT<T> = {
+  tag: 'hasT'
+  value: T
+}
+
+type NotHasT = {
+  tag: 'notHasT'
+}
+
+type Option<T> = HasT<T> | NotHasT
+
+const logNum = (obj: Option<number>) => {
+  if (obj.tag === 'hasT') {
+    console.log(obj.value)
+  } else {
+    console.log(`it is not a number`)
+  }
+}
+
+logNum({
+  tag: 'hasT',
+  value: 4
+})
+
+logNum({
+  tag: 'notHasT'
+})
