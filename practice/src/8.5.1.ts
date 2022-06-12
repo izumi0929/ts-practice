f7be370688c41c9f1dcc238633c303dc439069dc
izumeeeel <izumi.watahiki@gmail.com>
2022-06-12 20:58:14 +0900

    add practice2

--- practice/src/8.5.1.ts
+++ practice/src/8.5.1.ts
@@ -8,7 +8,23 @@ console.log({ __dirName })
 const uhyoPath = path.join(__dirName, '../uhyo.txt')
 console.log({ uhyoPath })
 
-const data = await readFile(uhyoPath, { encoding: 'utf8' })
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
+
+const errorAfter1ms = async () => {
+  await sleep(1)
+  throw new Error('time over yade')
+}
+
+const data = await Promise.race([
+  readFile(uhyoPath, { encoding: 'utf8' }),
+  errorAfter1ms()
+]).catch(() => {
+  return ''
+})
 
 let count = 0
 let currentIndex = 0
@@ -22,6 +38,35 @@ while (true) {
   }
 }
 console.log(count)
+
+// const sleepReject = (duration: number) => {
+//   return new Promise<never>((_resolve, reject) => {
+//     setTimeout(reject, duration)
+//   })
+// }
+
+// try {
+//   const data = Promise.race([
+//     await readFile(uhyoPath, { encoding: 'utf8' }),
+//     sleepReject(1)
+//   ])
+//   data.then((result) => {
+//     let count = 0
+//     let currentIndex = 0
+//     while (true) {
+//       const nextIndex = result.indexOf('uhyo', currentIndex)
+//       if (nextIndex >= 0) {
+//         count++
+//         currentIndex = nextIndex + 1
+//       } else {
+//         break
+//       }
+//     }
+//     console.log(count)
+//   })
+// } catch (e) {
+//   console.log('time over', e)
+// }
 
 // readFile(uhyoPath, 'utf8', (err, data) => {
 //   if (err) {
